c615083098a0a9333849657d0097dba7be92551a
Lludion <45879010+Lludion@users.noreply.github.com>
2020-10-13 10:25:37 +0200

    Adding raw code
    - now only in one file, will be organized
    - tests are in another file

---
+++ test.py
@@ -0,0 +1,26 @@
+###TESTS FOR QGOL
+from qgol import *
+
+## Cube tests
+print("Cube created : ")
+c = Cube(Cell)
+print(c)
+print("Active cells initially :")
+print(len(c))
+print("Activation of a Cell:")
+c.activation(0,0,1)
+print(c)
+print("Making one step on the cube:")
+c.op = QGOL_U()
+c.f()
+print(c)
+## subscript tests
+
+c[0,0,0] = Cell(True)
+print(c)
+
+## qGOL tests
+
+qg = QGOL()
+
+print(qg.s.cs)
