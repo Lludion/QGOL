a763b4f59b66d18713907e83e5e859164cf4a3c9
lludion <ulysse.remond@outlook.fr>
2020-10-19 18:48:14 +0200

    Corrected a bug in logging
    - logi and logd functions were not taking an arbitrary number of arguments
    - improved the test file

--- log/log.py
+++ log/log.py
@@ -1,15 +1,15 @@
 import logging as logg
 
 logg.basicConfig(filename='log/qgol.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',level=logg.INFO)
-logg.debug('Logging initialized.')
+logg.info('Logging initialized.')
 
 def logd(func):
 	"""
 	Wrapper to check when a function is entered or exited - outputs as debug
 	"""
-	def func_wrapper(name):
+	def func_wrapper(*ar,**arg):
 		logg.debug("Entering function " + str(func_wrapper.__name__))
-		z = func(name)
+		z = func(*ar,**arg)
 		logg.debug("Exiting function " + str(func_wrapper.__name__))
 		return z
 	func_wrapper.__name__ = func.__name__
@@ -19,9 +19,9 @@ def logi(func):
 	"""
 	Wrapper to check when a function is entered or exited - outputs as an info
 	"""
-	def func_wrapper(name):
+	def func_wrapper(*ar,**arg):
 		logg.info("Entering function " + str(func_wrapper.__name__))
-		z = func(name)
+		z = func(*ar,**arg)
 		logg.info("Exiting function " + str(func_wrapper.__name__))
 		return z
 	func_wrapper.__name__ = func.__name__
--- log/logtests.py
+++ log/logtests.py
@@ -1,20 +1,27 @@
 try:
-	from log.log import log
+	from log.log import logg,logd,logi
 except:
-	from log import log
+	from log import logg,logd,logi
 	
-@logd
+@logi
 def z(x):
 	print("Z",x)
 
-@logd
+@logi
 def f(x):
 	print("lkjlk",x)
 	z(ff)
 
-@logd
+@logi
 def ff(x):
 	print(f(f))
 
-f(z)
+ff(f(z))
 
+
+@logi
+def akayabu(tau,pi,lambdaprime):
+	print(tau,pi)
+	print(lambdaprime(tau))
+
+akayabu(akayabu,f,f)
