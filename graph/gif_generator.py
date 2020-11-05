9dac6a1fe2cc57b9e2a7aa4e22305debe781d9fc
Jean-Michel Costes <marin.costes@laposte.net>
2020-11-05 19:30:50 +0100

    Changement des arguments de generer_gif

--- graph/gif_generator.py
+++ graph/gif_generator.py
@@ -10,17 +10,28 @@ from obj import *
 from graph.display import *
 from os import environ
 
-def generer_gif(qgol,nb_step,name,scale=(0,0,0)):
-    if scale[0]==0:
+
+#Autoscale permet de fixer automatiquement une échelle adaptée
+#Si on ne précise ni la valeur de autoscale ni la valeur de scale 
+#l'échelle est "dynamique"
+def generer_gif(qgol,nb_step,name,scale=(0,0,0),autoscale=False):
+    if autoscale:
         copy = qgol.copy()
         for i in range(nb_step):
             copy.next()
         scl=afficher_qgol(copy,show=False,save=name+"copy")
     else:
         scl=scale
-    for i in range(nb_step):
-        afficher_qgol(qgol,show=False,save=name+"%d" %i,scale=scl)
-        qgol.next()
+    
+    if scale[0]==0:
+        for i in range(nb_step):
+            afficher_qgol(qgol,show=False,save=name+"%d" %i)
+            qgol.next()
+        
+    else:
+        for i in range(nb_step):
+            afficher_qgol(qgol,show=False,save=name+"%d" %i,scale=scl)
+            qgol.next()
     return
 
 qgol = QGOL()
